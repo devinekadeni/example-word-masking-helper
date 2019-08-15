9fe1fdf2de7cb40284e4db08b7165677a7d31af4
Devin <devin@getcraft.com>
2019-08-15 14:55:07 +0700

    added email phone masking helper function

---
+++ index.js
@@ -0,0 +1,70 @@
+const maskingEmail = (fullText, replacement = '*') => {
+  const regexEmail = /([a-z0-9_.-]+)@([da-z.-]+)\.([a-z.]{2,6})/gm
+  const listEmail = fullText.match(regexEmail)
+
+  if (listEmail && listEmail.length) {
+    // new list of masked email
+    const newListEmail = listEmail.map(email => {
+      const [name, mail] = email.split('@')
+
+      // Masking the name of email
+      const slicedName = name.length > 3 ? name.slice(1, name.length - 1) : name
+      const maskedName = name.replace(slicedName, replacement.repeat(slicedName.length))
+
+      // Masking the mail of email
+      const slicedEmail = mail.length > 3 ? mail.slice(1, mail.length - 3) : mail
+      const maskedMail = mail.replace(slicedEmail, replacement.repeat(slicedEmail.length))
+
+      return `${maskedName}@${maskedMail}`
+    })
+
+    let maskedEmail = fullText
+
+    // changing the email that contained on fulltext into the masked email
+    listEmail.forEach((email, i) => {
+      maskedEmail = maskedEmail.split(listEmail[i]).join(newListEmail[i])
+    })
+
+    return maskedEmail
+  }
+
+  return fullText
+}
+
+const maskingPhone = (fullText, replacement = '*') => {
+  const regexPhone = /[+]*[(]?[+]?[0-9]{1,4}[)]?[-\s./0-9]*/gm
+  const listPhone = fullText.match(regexPhone)
+
+  if (listPhone && listPhone.length) {
+    // new list of masked phone
+    const newListPhone = listPhone.map(phone => {
+      const slicedPhone = phone.slice(3, phone.length - 2)
+      const maskedPhone = phone.replace(
+        slicedPhone,
+        replacement.repeat(slicedPhone.length)
+      )
+
+      return maskedPhone
+    })
+
+    let maskedPhone = fullText
+
+    // changing the phonenumber that contained on fulltext into the masked phonenumber
+    listPhone.forEach((phone, i) => {
+      maskedPhone = maskedPhone.split(listPhone[i]).join(newListPhone[i])
+    })
+
+    return maskedPhone
+  }
+
+  return fullText
+}
+
+const maskingEmailPhone = (fullText, replacement = '*') => {
+  const maskedEmail = maskingEmail(fullText, replacement)
+  return maskingPhone(maskedEmail, replacement)
+}
+
+module.exports = maskingEmailPhone
+module.exports.maskingEmail = maskingEmail
+module.exports.maskingPhone = maskingPhone
